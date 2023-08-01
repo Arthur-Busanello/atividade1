436b943fbbf8e105d430f1817aba20fe43341e9f
ARTHUR BUSANELLO <tuti.busanello@hotmail.com>
2023-08-01 10:09:52 -0300

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,73 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner myInput = new Scanner(System.in);
+        List<Pessoas> pessoas = new ArrayList<>();
+
+        int option;
+        boolean exit = false;
+
+        do {
+            System.out.println("\nBem-vindo(a)");
+            System.out.println("1 - Cadastrar nome, idade e até 2 endereços");
+            System.out.println("2 - Listar pessoas e endereços");
+            System.out.println("3 - Pesquisar pessoas");
+            System.out.println("4 - Sair");
+
+            option = myInput.nextInt();
+            myInput.nextLine();
+
+            switch (option) {
+                case 1:
+                    System.out.println("Cadastre um nome, idade e até 2 endereços.");
+
+                    System.out.print("Nome: ");
+                    String nome = myInput.nextLine();
+
+                    System.out.print("Idade: ");
+                    int idade = myInput.nextInt();
+                    myInput.nextLine();
+
+                    List<Endereco> enderecos = new ArrayList<>();
+                    for (int i = 0; i < 2; i++) {
+                        System.out.print("Endereço " + (i + 1) + " - Rua: ");
+                        String rua = myInput.nextLine();
+
+                        System.out.print("Endereço " + (i + 1) + " - Número: ");
+                        int numero = myInput.nextInt();
+                        myInput.nextLine();
+
+                        enderecos.add(new Endereco(rua,numero));
+                    }
+
+                    Pessoas.cadastrarPessoa(nome, idade, enderecos,pessoas);
+                    break;
+
+                case 2:
+                    System.out.println("Listando pessoas e endereços:");
+                    Pessoas.listarPessoas(pessoas);
+                    break;
+
+                case 3:
+                    System.out.println("Digite o nome para pesquisar:");
+                    String searchName = myInput.nextLine();
+                    Pessoas.pesquisarPessoas(searchName,pessoas);
+                    break;
+
+                case 4:
+                    System.out.println("Encerrando atividades...");
+                    exit = true;
+                    break;
+
+                default:
+                    System.out.println("Opção inválida");
+                    break;
+            }
+        } while (!exit);
     }
 }
--- src/main/java/org/example/Pessoas.java
+++ src/main/java/org/example/Pessoas.java
@@ -1,2 +1,86 @@
-package org.example;public class Pessoas {
+package org.example;
+
+import java.util.List;
+
+public class Pessoas {
+    private String nome;
+    private int idade;
+    private List<Endereco> enderecos;
+
+    public Pessoas(String nome, int idade, List<Endereco> enderecos) {
+        this.nome = nome;
+        this.idade = idade;
+        this.enderecos = enderecos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+    public static void cadastrarPessoa(String nome, int idade, List<Endereco> enderecos, List<Pessoas> pessoas) {
+        pessoas.add(new Pessoas(nome, idade, enderecos));
+    }
+
+
+    public static void listarPessoas(List<Pessoas> pessoas) {
+        for (Pessoas pessoa : pessoas) {
+            System.out.println("Nome: " + pessoa.getNome());
+            System.out.println("Idade: " + pessoa.getIdade());
+
+            List<Endereco> enderecos = pessoa.getEnderecos();
+            for (int i = 0; i < enderecos.size(); i++) {
+                Endereco endereco = enderecos.get(i);
+                System.out.println("Endereço " + (i + 1) + ":");
+                System.out.println("  Rua: " + endereco.getRua());
+                System.out.println("  Número: " + endereco.getNumero());
+            }
+
+            System.out.println();
+        }
+    }
+    public static void pesquisarPessoas(String searchName, List<Pessoas> pessoas) {
+        boolean found = false;
+
+        for (Pessoas pessoa : pessoas) {
+            if (pessoa.getNome().equalsIgnoreCase(searchName)) {
+                System.out.println("}-CLIENTE ENCONTRADO-{");
+                System.out.println("Nome: " + pessoa.getNome());
+                System.out.println("Idade: " + pessoa.getIdade());
+
+                List<Endereco> enderecos = pessoa.getEnderecos();
+                for (int i = 0; i < enderecos.size(); i++) {
+                    Endereco endereco = enderecos.get(i);
+                    System.out.println("Endereço " + (i + 1) + ":");
+                    System.out.println("  Rua: " + endereco.getRua());
+                    System.out.println("  Número: " + endereco.getNumero());
+                }
+
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("}-CLIENTE NÃO ENCONTRADO-{");
+        }
+    }
 }
